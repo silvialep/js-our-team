4dc8e95043c98dd40ae756c2d8aebac5c6007c8b
silvialep <silvia.lepore_sl@libero.it>
2023-03-08 15:12:41 +0100

    feat: milestones 0 and 1 done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 /*
 Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
@@ -18,10 +15,61 @@ E ricordiamoci che console.log() è nostro amico!
 
 
 
-Wayne Barnett	  Founder & CEO	         wayne-barnett-founder-ceo.jpg
-Angela Caroll	  Chief Editor	         angela-caroll-chief-editor.jpg
-Walter Gordon	  Office Manager	     walter-gordon-office-manager.jpg
-Angela Lopez	  Social Media Manager	 angela-lopez-social-media-manager.jpg
-Scott Estrada	  Developer	             scott-estrada-developer.jpg
-Barbara Ramos	  Graphic Designer	     barbara-ramos-graphic-designer.jpg
 */
+
+
+
+// creo l’array di oggetti con le informazioni fornite
+
+let myTeam = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+
+console.log(myTeam);
+
+
+
+// stampo su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+for(i = 0; i < myTeam.length; i++) {
+    for(let key in myTeam[i]) {
+        console.log(`${key}: ${myTeam[i][key]}`);
+    }
+}
+
+
+
+
+
+// stampo le stesse informazioni su DOM sottoforma di stringhe
